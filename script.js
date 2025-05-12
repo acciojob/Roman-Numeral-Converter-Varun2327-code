1e1dd88ed2a6def6fa9763084e834a29cc930716
Varun shrimal <varunshrimal07@gmail.com>
2025-05-12 12:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-function convertToRoman(num) {
-  if (num < 1 || num > 3999) {
-    return "Out of range";
-  }
-
+/**
+ * Converts an integer to a Roman numeral.
+ *
+ * @param {number} num The integer to convert.
+ * @returns {string} The Roman numeral representation of the integer.
+ *
+ * Input range: 0 <= num <= 100000
+ */
+function intToRoman(num) {
+  // Define the mapping of Roman numerals to their integer values, including subtractive cases.
   const romanMap = [
-    ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
-    ['C', 100],  ['XC', 90],  ['L', 50],  ['XL', 40],
-    ['X', 10],   ['IX', 9],   ['V', 5],   ['IV', 4],
+    ['M', 1000],
+    ['CM', 900], // Added for optimization
+    ['D', 500],
+    ['CD', 400], // Added for optimization
+    ['C', 100],
+    ['XC', 90], // Added for optimization
+    ['L', 50],
+    ['XL', 40], // Added for optimization
+    ['X', 10],
+    ['IX', 9],  // Added for optimization
+    ['V', 5],
+    ['IV', 4],  // Added for optimization
     ['I', 1]
   ];
 
   let result = '';
 
-  for (let [symbol, value] of romanMap) {
+  // Iterate through the mapping and subtract the largest possible values from the input number.
+  for (let i = 0; i < romanMap.length; i++) {
+    const [symbol, value] = romanMap[i];
     while (num >= value) {
       result += symbol;
       num -= value;
     }
   }
-
   return result;
 }
-
-// Test examples
-console.log(convertToRoman(14));   // Output: XIV
-console.log(convertToRoman(798));  // Output: DCCXCVIII
