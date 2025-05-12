42c03991f1fbd63c5e54a15ce73df900ede9fe5c
Varun shrimal <varunshrimal07@gmail.com>
2025-05-12 13:28:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-function intToRoman(num) {
-  const romanMap = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-  ];
+function convertToRoman(num) {
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
 
-  let result = '';
-  for (const [symbol, value] of romanMap) {
-    while (num >= value) {
-      result += symbol;
-      num -= value;
-    }
-  }
-  return result;
+
+let n=798;
+let ans="";
+while(n!=0){
+   
+   for(let i in romanMap){
+
+       let value=romanMap[i][0];
+       let num=romanMap[i][1];
+
+
+        if(num<=n){
+
+           ans=ans+value;
+            n=n-num;
+             break;
+            
+}
+        
+}
+
 }
